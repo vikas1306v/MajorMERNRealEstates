469dc697aaf94249eb678f8630c0e7a9b52cb095
Vikas Verma <99089201+vikas1306v@users.noreply.github.com>
2023-12-05 18:51:32 +0530

    Update firebase.jsx

--- my-project-mern-major/src/firebase.jsx
+++ my-project-mern-major/src/firebase.jsx
@@ -1,15 +1,6 @@
 
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBMsD-r5lbi8FUeU3c7vk82zILziO7Z5qs",
-  authDomain: "real-state-4d214.firebaseapp.com",
-  projectId: "real-state-4d214",
-  storageBucket: "real-state-4d214.appspot.com",
-  messagingSenderId: "668918161783",
-  appId: "1:668918161783:web:8e7ebd767e058ce6ba8172",
-  measurementId: "G-MEE7XN6BZY"
-};
 
 const app = initializeApp(firebaseConfig);
 export default app;
